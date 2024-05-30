aacbd525b7dfce7a466e5be3312d871b59a673b1
sethspr <seth.c.spring@gmail.com>
2024-05-30 13:16:08 -0400

    auto generated

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -7,18 +7,20 @@ public class Graph : MonoBehaviour
     [SerializeField]
     Transform pointPrefab;
 
-    [SerializeField]
+    [SerializeField, Range(10, 100)]
     int resolution = 10;
 
     void Awake () {
+        float step = 2f / resolution;
         var position = Vector3.zero;
-		var scale = Vector3.one / 5f;
-		for (int i = 0; i < 10; i++) {
+		var scale = Vector3.one * step;
+		for (int i = 0; i < resolution; i++) {
 			Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) / 5f - 1f;
+            position.x = (i + 0.5f) * step - 1f;
             position.y = position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform, false);
 		}
     }
 }
