a539003abe46ede60d471618c0c0bf04de677d66
sethspr <seth.c.spring@gmail.com>
2024-05-30 13:02:34 -0400

    object iteration for graph

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+
+    [SerializeField]
+    int resolution = 10;
+
+    void Awake () {
+        var position = Vector3.zero;
+		var scale = Vector3.one / 5f;
+		for (int i = 0; i < 10; i++) {
+			Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) / 5f - 1f;
+            position.y = position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+		}
+    }
+}
