0834112d7470dd203c06d960d31199769a047530
sethspr <seth.c.spring@gmail.com>
2024-05-30 15:52:02 -0400

    update points for animation

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -10,17 +10,28 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    Transform[] points;
+
     void Awake () {
         float step = 2f / resolution;
         var position = Vector3.zero;
 		var scale = Vector3.one * step;
-		for (int i = 0; i < resolution; i++) {
-			Transform point = Instantiate(pointPrefab);
+        points = new Transform[resolution];
+		for (int i = 0; i < points.Length; i++) {
+			Transform point = points[i] =Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
 		}
+    }
+
+    void Update () {
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+        }
     }
 }
